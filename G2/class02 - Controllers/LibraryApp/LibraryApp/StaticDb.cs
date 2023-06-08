4d3b72ac81d880b4c80d3d2fb2350a3f8c695cb1
Viktor Jakovlev <vicebad@hotmail.com>
2023-06-08 20:38:17 +0200

    class 02 updated

---
+++ G2/class02 - Controllers/LibraryApp/LibraryApp/StaticDb.cs
@@ -0,0 +1,26 @@
+﻿using LibraryApp.Models;
+
+namespace LibraryApp
+{
+    public static class StaticDb
+    {
+        public static List<Book> Books = new List<Book>()
+        {
+            new Book 
+            {
+                Id = 1,
+                Title = "Kasni Porasni"
+            },
+            new Book
+            {
+                Id = 2,
+                Title = "Zoki Poki"
+            },
+            new Book
+            {
+                Id = 3,
+                Title = "35 Maj"
+            }
+        };
+    }
+}
