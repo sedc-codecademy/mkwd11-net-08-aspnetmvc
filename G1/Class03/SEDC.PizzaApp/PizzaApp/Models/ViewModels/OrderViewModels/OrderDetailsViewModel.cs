87186aaa67dd7f29d73ec88ba953268601e1e3b7
Bojan Damchevski <bod@proofficepark.dk>
2023-06-13 01:17:52 +0200

    code

---
+++ G1/Class03/SEDC.PizzaApp/PizzaApp/Models/ViewModels/OrderViewModels/OrderDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using PizzaApp.Models.Enums;
+
+namespace PizzaApp.Models.ViewModels.OrderViewModels
+{
+    public class OrderDetailsViewModel
+    {
+        public string PizzaName { get; set; } = string.Empty;
+
+        public string UserFullName { get; set; } = string.Empty;
+
+        public int Price { get; set; }
+
+        public PaymentMethod PaymentMethod { get; set; }
+    }
+}
