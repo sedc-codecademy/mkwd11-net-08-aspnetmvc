8b247b9df682b204ba24a689af7314dcd753bfc2
Viktor Jakovlev <vicebad@hotmail.com>
2023-06-13 19:44:24 +0200

    library app finished

--- G2/LibraryApp/LibraryApp/Controllers/BookController.cs
+++ G2/LibraryApp/LibraryApp/Controllers/BookController.cs
@@ -1,4 +1,5 @@
 ﻿using LibraryApp.Models;
+using LibraryApp.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryApp.Controllers
@@ -48,16 +49,16 @@ namespace LibraryApp.Controllers
 
         //https://localhost:7100/book/details/1 => route parameter
         //https://localhost:7100/book/details?id=1 => query parameter
-        public IActionResult Details(int? id) 
+        public IActionResult Details(int? id)
         {
-            if (id == null) 
+            if (id == null)
             {
                 return new EmptyResult();
             }
 
             Book book = StaticDb.Books.FirstOrDefault(book => book.Id == id);
 
-            if (book == null) 
+            if (book == null)
             {
                 return new EmptyResult();
             }
@@ -66,9 +67,66 @@ namespace LibraryApp.Controllers
         }
 
         [Route("deletebook/{id?}/filter/{filterParam?}")]
-        public IActionResult Delete(int? id, string? filterParam) 
+        public IActionResult Delete(int? id, string? filterParam)
         {
             return View();
+        }
+
+        //View Data
+        public IActionResult ViewDataExample()
+        {
+            ViewData.Add("randomData", "some random view data");
+
+            var book = new Book
+            {
+                Id = 123,
+                Title = "Test Book 1"
+            };
+
+            //not wirking in view data
+            ViewData.Add("book", book);
+
+            return View();
+        }
+
+        //View Bag
+        public IActionResult ViewBagExample() 
+        {
+            ViewBag.RandomData = "some random view bag data";
+
+            var book = new Book
+            {
+                Id = 123,
+                Title = "Test Book 1"
+            };
+
+            ViewBag.Book = book;
+
+            return View();
+        }
+
+        //View Model
+        public IActionResult ViewModelExample() 
+        {
+            var book = new Book
+            {
+                Id = 123,
+                Title = "Test Book 1"
+            };
+
+            var comic = new Comic
+            {
+                Id = 125,
+                ComicTitle = "Test Comic 1"
+            };
+
+            var viewModel = new BookComicViewModel
+            {
+                Book = book,
+                Comic = comic
+            };
+
+            return View(viewModel);
         }
 
 
---
+++ G2/LibraryApp/LibraryApp/Models/Comic.cs
@@ -0,0 +1,8 @@
+﻿namespace LibraryApp.Models
+{
+    public class Comic
+    {
+        public int Id { get; set; }
+        public string ComicTitle { get; set; }
+    }
+}
