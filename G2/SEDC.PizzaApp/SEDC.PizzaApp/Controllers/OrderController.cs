5328f693ed7b4cc3d03675892b1c5936170ae427
Viktor Jakovlev <vicebad@hotmail.com>
2023-06-15 19:53:17 +0200

    updates

---
+++ G2/SEDC.PizzaApp/SEDC.PizzaApp/Controllers/OrderController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models.Domain;
+using SEDC.PizzaApp.Models.ViewModels;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Order> ordersFromDb = StaticDb.Orders;
+
+            //==MANUAL MAPPING==
+            //List<OrderViewModel> orderViewModels = new List<OrderViewModel>();
+
+            //foreach (Order order in ordersFromDb) 
+            //{
+            //    var temporaryOrderViewModel = new OrderViewModel()
+            //    {
+            //        PizzaName = order.Pizza.Name,
+            //        UserFullName = $"{order.User.FirstName} {order.User.LastName}",
+            //        PaymentMethod = order.PaymentMethod,
+            //        Price = order.Pizza.Price
+            //    };
+
+            //    orderViewModels.Add(temporaryOrderViewModel);
+            //}
+
+            //==MANUAL MAPING WITH LINQ
+            List<OrderViewModel> orderViewModels = ordersFromDb.Select((order) => new OrderViewModel
+            {
+                PizzaName = order.Pizza.Name,
+                UserFullName = $"{order.User.FirstName} {order.User.LastName}",
+                PaymentMethod = order.PaymentMethod,
+                Price = order.Pizza.Price
+            }).ToList();
+
+            return View(orderViewModels);
+        }
+    }
+}
---
+++ G2/SEDC.PizzaApp/SEDC.PizzaApp/Models/ViewModels/OrderViewModel.cs
@@ -0,0 +1,12 @@
+﻿using SEDC.PizzaApp.Models.Enums;
+
+namespace SEDC.PizzaApp.Models.ViewModels
+{
+    public class OrderViewModel
+    {
+        public string PizzaName { get; set; }
+        public string UserFullName { get; set; }
+        public int Price { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+    }
+}
