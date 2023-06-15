775280a6171fb0867a7131436a27df1a8e485b17
djamtoski <mdjamtoski@gmail.com>
2023-06-15 14:25:42 +0200

    Class - 02

---
+++ G8/Class02 - Controllers/PizzaApp/SEDC.PizzaApp/SEDC.PizzaApp/Controllers/PizzaController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult GetPizzas()
+        {
+            List<Pizza> dbPizzas = StaticDb.Pizzas;
+            return View(dbPizzas);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return RedirectToAction("Error");
+            }
+
+            Pizza pizza = StaticDb.Pizzas.FirstOrDefault(x => x.Id == id);
+            if(pizza == null)
+            {
+                return RedirectToAction("Error");
+            }
+
+            return View(pizza);
+        }
+
+        [Route("SeePizzas")] //localhost:[port]/SeePizzas
+        public IActionResult Redirect()
+        {
+            return RedirectToAction("GetPizzas");
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+    }
+}
