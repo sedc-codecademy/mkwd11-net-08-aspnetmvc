dac0f513f77e4d6c4a7063325928729547f4d874
Jovan Gjorgjiev <joki.joksi@hotmail.com>
2023-06-19 18:24:09 +0200

    Class 05

---
+++ G3/Class06/SEDC.PizzaApp/SEDC.PizzaApp.Web/Models/Domain/Pizza.cs
@@ -0,0 +1,21 @@
+﻿namespace SEDC.PizzaApp.Web.Models.Domain
+{
+    public class Pizza
+    {
+        public Pizza(Order order, string name, decimal price)
+        {
+            Order = order ?? throw new ArgumentNullException(nameof(order));
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Price = price;
+        }
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+
+        public bool IsOnPromotion { get; set; }
+
+        public Order Order { get; set; }
+    }
+}
