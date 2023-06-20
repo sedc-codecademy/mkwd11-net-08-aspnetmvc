65b85510ee12c68532a8c18a44f4dd6d13a90e99
tijanastojanovska <stojanovska_tijana@outlook.com>
2023-06-20 20:53:42 +0200

    Class 05

---
+++ G5/Class 05/PizzaApp/PizzaApp/Controllers/PizzaController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaApp.Models.Domain;
+
+namespace PizzaApp.Controllers
+{
+    public class PizzaController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetAllPizzas()
+        {
+            List<Pizza> pizzas = StaticDb.Pizzas;
+            return View(pizzas);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return RedirectToAction("Error");
+            }
+
+            Pizza pizza = StaticDb.Pizzas.FirstOrDefault(x => x.Id == id);
+
+            if(pizza == null)
+            {
+                // return RedirectToAction("Error");
+                return View("ResourceNotFound");
+            }
+
+            return View(pizza);
+
+
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
