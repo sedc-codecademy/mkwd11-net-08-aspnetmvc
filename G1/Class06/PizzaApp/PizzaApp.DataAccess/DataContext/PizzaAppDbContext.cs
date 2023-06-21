2585f23494cae20e9c0b0fc2289026c00cba2a2f
Bojan Damchevski <bod@proofficepark.dk>
2023-06-21 18:39:01 +0200

    pizzas

---
+++ G1/Class06/PizzaApp/PizzaApp.DataAccess/DataContext/PizzaAppDbContext.cs
@@ -0,0 +1,65 @@
+﻿namespace PizzaApp.DataAccess.DataContext
+{
+    using Microsoft.EntityFrameworkCore;
+    using PizzaApp.Domain.Models;
+
+    public class PizzaAppDbContext : DbContext
+    {
+        public PizzaAppDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Pizza> Pizzas { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Order>()
+                .HasMany(x => x.PizzaOrders)
+                .WithOne(x => x.Order)
+                .HasForeignKey(x => x.OrderId);
+
+            modelBuilder.Entity<Pizza>()
+                .HasMany(x => x.PizzaOrders)
+                .WithOne(x => x.Pizza)
+                .HasForeignKey(x => x.PizzaId);
+
+            modelBuilder.Entity<User>()
+                .HasMany(x => x.Orders)
+                .WithOne(x => x.User)
+                .HasForeignKey(x => x.UserId);
+
+            modelBuilder.Entity<Pizza>()
+                .HasData(new Pizza
+                {
+                    Id = 1,
+                    IsOnPromotion = false,
+                    Name="Capriciosa"
+                },
+                new Pizza
+                {
+                    Id = 2,
+                    IsOnPromotion = true,
+                    Name = "Oliva"
+                },
+                new Pizza
+                {
+                    Id = 3,
+                    IsOnPromotion = true,
+                    Name = "Mexicana"
+                },
+                new Pizza
+                {
+                    Id = 4,
+                    IsOnPromotion = false,
+                    Name = "Pepperoni"
+                });
+        }
+    }
+}
