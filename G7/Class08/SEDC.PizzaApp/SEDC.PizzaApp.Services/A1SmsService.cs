160ef0feb72cdca04eb522a2e16874e0b599dc83
risto-panchevski <76558034+risto-panchevski@users.noreply.github.com>
2023-06-29 20:55:12 +0200

    Class 08

---
+++ G7/Class08/SEDC.PizzaApp/SEDC.PizzaApp.Services/A1SmsService.cs
@@ -0,0 +1,17 @@
+﻿using SEDC.PizzaApp.Services.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.Services
+{
+    public class A1SmsService : ISmsService
+    {
+        public string Send(string phoneNumber, string message)
+        {
+            return $"Using A1 sending sms to {phoneNumber}: {message}";
+        }
+    }
+}
---
+++ G7/Class08/SEDC.PizzaApp/SEDC.PizzaApp.Services/Abstraction/ISizeService.cs
@@ -0,0 +1,12 @@
+﻿using SEDC.PizzaApp.ViewModels;
+
+namespace SEDC.PizzaApp.Services.Abstraction
+{
+    public interface ISizeService
+    {
+        List<SizeViewModel> GetAll();
+        SizeViewModel GetById(int id);
+        void Save(SizeViewModel model);
+        void Remove(int id);
+    }
+}
