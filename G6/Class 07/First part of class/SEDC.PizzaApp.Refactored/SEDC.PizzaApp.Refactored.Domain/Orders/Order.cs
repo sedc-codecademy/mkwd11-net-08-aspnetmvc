a59d9d350c9c4a0e9e293eb272b38cbb2240c4a8
tstojanovska <stojanovskatanja03@gmail.com>
2023-06-29 18:46:38 +0200

    First part class 07

---
+++ G6/Class 07/First part of class/SEDC.PizzaApp.Refactored/SEDC.PizzaApp.Refactored.Domain/Orders/Order.cs
@@ -0,0 +1,19 @@
+﻿using SEDC.PizzaApp.Refactored.Domain.Enums;
+using SEDC.PizzaApp.Refactored.Domain.Users;
+
+namespace SEDC.PizzaApp.Refactored.Domain.Orders
+{
+    public class Order : BaseEntity
+    {
+        //FK column
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        //column (int)
+        public PaymentMethod PaymentMethod { get; set; }
+
+        //column (bit)
+        public bool IsDelivered { get; set; }
+        public List<PizzaOrder> PizzaOrders { get; set; }
+    }
+}
---
+++ G6/Class 07/First part of class/SEDC.PizzaApp.Refactored/SEDC.PizzaApp.Refactored.Services/Implementations/OrderService.cs
@@ -0,0 +1,37 @@
+﻿using SEDC.PizzaApp.Refactored.DataAccess.Interfaces;
+using SEDC.PizzaApp.Refactored.Domain.Orders;
+using SEDC.PizzaApp.Refactored.Services.Interfaces;
+using SEDC.PizzaApp.Refactored.ViewModels.Orders;
+using SEDC.PizzaApp.Refactored.Mappers.Orders;
+
+namespace SEDC.PizzaApp.Refactored.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        
+        //service (business layer) needs data layer
+        private IRepository<Order> _orderRepository;
+
+        //we need a method that:
+        //reads from db -> we need a class that directly communicates with db (Repository)
+        //map to viewmodels
+        public List<OrderListViewModel> GetAllOrders()
+        {
+            //from db we get list of  domain models
+            List<Order> ordersDb = _orderRepository.GetAll();
+
+            //map to viewmodels
+            List<OrderListViewModel> orderListViewModels =
+                ordersDb.Select(x => x.ToOrderListViewModel()).ToList();
+
+            //List<OrderListViewModel> viewModels = new List<OrderListViewModel>();
+            //foreach(Order order in ordersDb)
+            //{
+            //    viewModels.Add(order.ToOrderListViewModel());
+            //}
+            //return viewModels;
+
+            return orderListViewModels;
+        }
+    }
+}
