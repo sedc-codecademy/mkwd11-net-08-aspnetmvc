96711dca840a02df97d1d4b32cbcb0582b4413c3
tstojanovska <stojanovskatanja03@gmail.com>
2023-07-04 21:00:27 +0200

    Class 06

---
+++ G6/Class 09/SEDC.PizzaApp.Refactored/SEDC.PizzaApp.Refactored.DataAccess/Implementations/PizzaRepository.cs
@@ -0,0 +1,33 @@
+﻿using SEDC.PizzaApp.Refactored.DataAccess.Interfaces;
+using SEDC.PizzaApp.Refactored.Domain.Pizzas;
+
+namespace SEDC.PizzaApp.Refactored.DataAccess.Implementations
+{
+    public class PizzaRepository : IRepository<Pizza>
+    {
+        public void DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Pizza> GetAll()
+        {
+            return StaticDb.Pizzas;
+        }
+
+        public Pizza GetById(int id)
+        {
+            return StaticDb.Pizzas.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Insert(Pizza entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Pizza entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ G6/Class 09/SEDC.PizzaApp.Refactored/SEDC.PizzaApp.Refactored.Domain/Users/User.cs
@@ -0,0 +1,19 @@
+﻿using SEDC.PizzaApp.Refactored.Domain.Orders;
+
+
+namespace SEDC.PizzaApp.Refactored.Domain.Users
+{
+    //all domain entities have Id, that is why they inherit from BaseEntity
+    public class User : BaseEntity
+    {
+        //these properties reflect columns from tables
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string? SSN { get; set; } 
+
+        //1 User - Many Orders
+        public List<Order> Orders { get; set; }
+    }
+}
